aa9e2751842fef5a61191ac6eb11a73438010750
ravikanthooda <ravikanthooda@outlook.com>
2025-11-03 19:32:53 +0530

    FindLargestInArray

---
+++ FindLargestInArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class FindLargestInArray
+{
+    static void Main()
+    {
+        int[] num = { 1, 5, 2, 6, 7, 3, 9, 4 };
+        int largestNum = 0;
+
+        for (int pass = 0; pass < num.Length - 1; pass++)
+        {
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] > largestNum)
+                {
+                    largestNum = num[i];
+                }
+            }
+        }
+        Console.WriteLine($"Largest Number is : {largestNum}");
+    }
+}
