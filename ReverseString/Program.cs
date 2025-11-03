95fca74e6ec933df2faaadedb3142efd10d7fce5
ravikanthooda <ravikanthooda@outlook.com>
2025-11-03 19:15:53 +0530

    ReverseString

---
+++ ReverseString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+class ReverseString
+{
+    static void Main()
+    {
+        string str = "Ravi";
+        StringBuilder revStr = new StringBuilder();
+        for (int i = str.Length-1; i >= 0; i--)
+        {
+            revStr.Append(str[i]);
+        }
+        Console.WriteLine(revStr);
+    }
+}
